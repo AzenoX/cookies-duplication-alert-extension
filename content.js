0950cf5b7c1538214717802068102a88d2203380
Alexis Hayat <azen0x.alexis@gmail.com>
2022-05-09 20:51:06 +0200

    feat: improve html generation using DocumentFragment

--- content.js
+++ content.js
@@ -25,42 +25,34 @@ document.cookie.replace(/\s/g, "").split(';').forEach(cookie => {
 
 // If there is duplicates, then show el cookie dance
 if (duplicates.length > 0) {
-    // Create main div element
-    const div = document.createElement('div');
-    div.style.display = 'flex';
-    div.style.alignItems = 'top';
-    div.style.position = 'fixed';
-    div.style.top = '60vh';
-    div.style.height = '10vh;';
-
-    // Create image tag
-    const img = document.createElement('img');
-    img.src = gifPath;
-    div.append(img);
-
-    // Create the bubble dialogue element
-    const bubbleClipPath = '8% 0%, 100% 0%, 100% 100%, 8% 100%, 8% 75%, 0% 70%, 8% 65%';
+    const bubbleClipPath = 'polygon(8% 0%, 100% 0%, 100% 100%, 8% 100%, 8% 75%, 0% 70%, 8% 65%)';
     const color = '#1E38A0';
-    const paragraph_wrapper = document.createElement('div');
-    paragraph_wrapper.style.padding = '0.2em';
-    paragraph_wrapper.style.background = color;
-    paragraph_wrapper.style.height = 'fit-content';
-    paragraph_wrapper.style.clipPath = 'polygon(' + bubbleClipPath + ')';
-    const paragraph = document.createElement('p');
-    paragraph.style.padding = '1em 3em';
-    paragraph.style.height = '100%';
-    paragraph.style.width = '100%';
-    paragraph.style.margin = '0';
-    paragraph.style.background = '#ffffff';
-    paragraph.style.color = color;
-    paragraph.style.clipPath = 'polygon(' + bubbleClipPath + ')';
-    paragraph.innerHTML = '<strong>Cookies are weird :/</strong><br/><br/>List of duplicated cookies:<br/>';
-    duplicates.forEach(el => {
-        paragraph.innerHTML += '● ' + el + '<br/>';
-    });
-    paragraph_wrapper.append(paragraph);
-    div.append(paragraph_wrapper);
 
-    // Then append cookie element to body
-    document.body.append(div);
+    const template = () => (`
+    <div id="cookie-dance-wrapper" style="display: flex; position: fixed; top: 60vh; height: 30vh;">
+        <img src="${gifPath}" alt="cookie gif">
+        <div style="padding: 0.2em; background: ${color}; height: fit-content; clip-path: ${bubbleClipPath}">
+            <div style="background: #ffffff; padding: 1em 3em; margin: 0; height: 100%; width: 100%; color: ${color}; clip-path: ${bubbleClipPath};">
+                <p style="font-weight: bold;">
+                    Cookies are buggy :/
+                </p>
+                <div style="padding: 0.2em;">
+                    <span>List of duplicated cookies:</span>
+                    <ul>
+                        ${duplicates.map(function (el) {
+                            return "<li>" + el + "</li>"
+                        }).join("")}
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    `);
+
+    const fragment = document.createDocumentFragment();
+    const parser = new DOMParser();
+    const cookieGif = parser.parseFromString(template(), 'text/html');
+    const els = cookieGif.documentElement.querySelector('#cookie-dance-wrapper');
+    fragment.appendChild(els);
+    document.body.appendChild(fragment);
 }
